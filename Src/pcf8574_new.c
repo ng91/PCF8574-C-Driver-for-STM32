303837cc179a01ce303f2f00c5eab4e8036ef90d
ng91 <nikoloz.glonti@o2.pl>
2019-06-08 15:08:18 +0200

    ADDED PROPER DESCRIPTION

--- Src/pcf8574_new.c
+++ Src/pcf8574_new.c
@@ -1,5 +1,6 @@
 /*
  * pcf8574.c
+         Library used to control a PCF8574 I/O expander by I2C bus - no support for reading ports yet...
  	 	 Biblioteka służąca do obsługi przy użyciu I2C ekspanderów opartych na PCF8574
  *
  *  Created on: 26.05.2019
@@ -13,7 +14,7 @@ I2C_HandleTypeDef hi2c1;
 
 	char str[10];
 	uint8_t pcf8574_addr[EXP_NUM];
-	uint8_t stan_portu[EXP_NUM];
+	uint8_t port_state[EXP_NUM];
 	uint8_t txdata[EXP_NUM];
 	int i;
 	int b = 0;
@@ -34,7 +35,7 @@ I2C_HandleTypeDef hi2c1;
 void pcf8574_init_0(uint8_t exp_num, uint8_t exp_addr){
 	pcf8574_addr[exp_num]=exp_addr;
 	for(i=0;i=2;i++){
-		stan_portu[EXP_NUM]=0b00000000;
+		port_state[EXP_NUM]=0b00000000;
 	}
 
 }
@@ -45,19 +46,19 @@ void pcf8574_setaddr(int exp_num, int addr){
 
 void pcf8574_write_0(int exp_num, int port_num, int port_state, int send_cmd){
 
-	uint8_t liczba=int_pow(2, port_num);
+	uint8_t number=int_pow(2, port_num);
 
-	uint8_t liczba_zanegowana =  ~liczba;
+	uint8_t number_negated =  ~number;
 
 	if(port_num<=7 || port_num>=0){
 		if(port_state == 1){
-			stan_portu[exp_num] = stan_portu[exp_num] | liczba;
-			txdata[exp_num] = stan_portu[exp_num];
+			port_state[exp_num] = port_state[exp_num] | number;
+			txdata[exp_num] = port_state[exp_num];
 
 		}
 		if(port_state == 0){
-			stan_portu[exp_num] = stan_portu[exp_num] & liczba_zanegowana;
-			txdata[exp_num] = stan_portu[exp_num];
+			port_state[exp_num] = port_state[exp_num] & number_negated;
+			txdata[exp_num] = port_state[exp_num];
 
 		}
 
@@ -65,13 +66,13 @@ void pcf8574_write_0(int exp_num, int port_num, int port_state, int send_cmd){
 
 	if(send_cmd==1){
 		uint8_t addr_send[1];
-		uint8_t stan_port_send[1];
-		stan_port_send[0] = txdata[exp_num];
+		uint8_t port_state_send[1];
+		port_state_send[0] = txdata[exp_num];
 		addr_send[0] = pcf8574_addr[exp_num];
 		uint8_t addr[1];
 		addr[0] = addr_send[0];
 		uint8_t tx[1];
-		uint8_t txdata = stan_port_send[0];
+		uint8_t txdata = port_state_send[0];
 		tx[0]= txdata;
 		HAL_I2C_Master_Transmit(&hi2c1,addr[0],tx,1,100);
 		//HAL_I2C_Master_Transmit_DMA(&hi2c1, addr[0], tx, 1);
